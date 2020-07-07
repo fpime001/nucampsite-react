1d364a1150d038350bc4bfd20ff4c302b3153b2c
Frank Pimentel <frank@elevatelocal.net>
2020-07-07 15:28:37 -0700

    Week 2 Workshop Assignment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-
 export default class CampsiteInfo extends Component {
   renderCampsite(campsite) {
     return (
@@ -17,9 +16,40 @@ export default class CampsiteInfo extends Component {
     );
   }
 
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => {
+            return (
+              <div key={comment.id}>
+                <p>
+                  {comment.text} <br />
+                  -- {comment.author},
+                  {new Intl.DateTimeFormat('en-US', {
+                    year: 'numeric',
+                    month: 'short',
+                    day: '2-digit',
+                  }).format(new Date(Date.parse(comment.date)))}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+    return <div />;
+  }
+
   render() {
     if (this.props.campsite) {
-      return <div className="row">{this.renderCampsite(campsite)}</div>;
+      return (
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
+        </div>
+      );
     }
     return <div />;
   }
