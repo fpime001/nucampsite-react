d69f94a513a1105461313dd14ac3f36299387aaf
Frank Pimentel <frank@elevatelocal.net>
2020-07-07 14:47:46 -0700

    error message on CcampsiteInfoComponent

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+
+export default class CampsiteInfo extends Component {
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.props.campsite) {
+      return <div className="row">{this.renderCampsite(campsite)}</div>;
+    }
+    return <div />;
+  }
+}
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 export default class Directory extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedCampsite: null,
+      selectedCampsite: null
     };
   }
 
@@ -14,21 +15,6 @@ export default class Directory extends Component {
     this.setState({
       selectedCampsite: campsite,
     });
-  }
-
-  renderSelectedCampsite(campsite) {
-    if (campsite) {
-      return (
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-    return <div />;
   }
 
   render() {
@@ -48,11 +34,7 @@ export default class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <div className="row">
-          <div className="col-md-5 m-1">
-            {this.renderSelectedCampsite(this.state.selectedCampsite)}
-          </div>
-        </div>
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     );
   }
