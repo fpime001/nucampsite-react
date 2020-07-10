885a0c461f047c23d420132d68e0858e4e804320
Frank Pimentel <frank@elevatelocal.net>
2020-07-10 15:37:54 -0700

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-export default class CampsiteInfo extends Component {
-  renderCampsite(campsite) {
+
+function RenderCampsite({campsite}) {
     return (
       <div className="col-md-5 m-1">
         <Card>
@@ -16,7 +16,7 @@ export default class CampsiteInfo extends Component {
     );
   }
 
-  renderComments(comments) {
+function RenderComments({comments}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -42,17 +42,18 @@ export default class CampsiteInfo extends Component {
     return <div />;
   }
 
-  render() {
-    if (this.props.campsite) {
+ function CampsiteInfo(props) {
+    if (props.campsite) {
       return (
         <div className="container">
           <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments={props.campsite.comments} />
           </div>
         </div>
       );
     }
     return <div />;
   }
-}
+
+  export default CampsiteInfo;
