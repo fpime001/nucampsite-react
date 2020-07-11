fc71627a5f0183d3b2d6e527d03d80ac4e39bab3
Frank Pimentel <frank@elevatelocal.net>
2020-07-11 12:01:35 -0700

    Week 3 Workshop Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import About from "./AboutComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
@@ -59,6 +60,7 @@ class Main extends Component {
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/about" render={() => <About partners={this.state.partners} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
